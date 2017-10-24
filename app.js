37957384e8d577112fcf13306fe03e736fb8f86d
AntoineBerger <antoine.berger@sciencespo.fr>
2017-10-24 16:05:40 +0200

    1

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Antoine",
+    lastName: "Berger",
+    description: "Je suis étudiant à l'école de journalisme de Sciences Po.",
+    photo: "",
+    twitter: "Ant_Berger",
+    github: "AntoineBerger",
+    mail: "antoine.bgr@gmail.com",
     experiences: [{
       dateBegin: "2014",
       dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
+      name: "Pôle Nature, Mondadori",
+      title: "Collaborateur pigiste",
+      logo: "http://snj-paris-idf.org/wp-content/uploads/logo-mondadori.jpg",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "Été 2013",
+      dateBegin: "Été 2017",
       dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      name: "Le Berry républicain",
+      title: "Journaliste",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      dateBegin: "Janvier 2017",
+      dateEnd: false,
+      name: "La Revue nationale de la chasse / Grand Gibier, Mondadori",
+      title: "Stagiaire",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
+      dateBegin: "2016",
+      dateEnd: "Today",
       name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      university: "École de journalisme de Sciences Po, Paris"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
-      name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2013",
+      dateEnd: "2016",
+      name: "Licence d'histoire",
+      university: "Université d'Orléans"
     }],
     languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    skills: ["Adobe Premiere", "Adobe Audition", "Adobe Lightroom"]
   },
   computed: {
     fullName() {
